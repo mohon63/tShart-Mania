8c0d104ad39a73fa06678306e2d84a71d14fb834
mohon63 <mohonhossen63@gmail.com>
2022-08-31 21:51:03 +0600

    Tshart map loop throgh

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import useTshirts from '../../hooks/useTshirts';
+import Cart from '../Cart/Cart';
+import Tshirt from '../Tshirt/Tshirt';
+import './Home.css';
 
 const Home = () => {
+    const [tShirts, setTShirts] = useTshirts();
     return (
-        <div>
-            <h2>This is home page</h2>
+        <div className='home-container'>
+            <div className="tshirt-container">
+                {
+                    tShirts.map(tShirt => <Tshirt
+                        tShirt={tShirt}
+                        key={tShirt._id}
+                    ></Tshirt>)
+                }
+            </div>
+            <div className="cart-container">
+                <Cart></Cart>
+            </div>
         </div>
     );
 };
