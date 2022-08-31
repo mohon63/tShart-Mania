a6a4345f2b43d045c3097b4cff90ef5a50eda91d
mohon63 <mohonhossen63@gmail.com>
2022-08-31 16:13:35 +0600

    tshart maina header, home and nav created

---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import CustomLink from '../CustomLInk/CustomLink';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <h2>Welcome to header for tShar Mania</h2>
+            <nav className='customlink'>
+                <CustomLink to='/'>Home</CustomLink>
+                <CustomLink to='/orderreview'>Order Review</CustomLink>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
